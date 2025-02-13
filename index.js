1fbb10ae209d6b5f812ee01fc9e33d5ac584ea64
tobymiles <ekwealortobechukwu22@gmail.com>
2025-02-13 20:35:15 +0100

    added loader

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 2000); // Adjust delay if needed
+};
+
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector("nav ul");
 menuToggle.addEventListener("click", () => {
@@ -14,12 +21,12 @@ menuToggle.addEventListener("click", () => {
 //     });
 //   });
 
-document
-  .getElementById("gallery-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.querySelector("#gallery").scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
+// document
+//   .getElementById("gallery-link")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     document.querySelector("#gallery").scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
+//   });
